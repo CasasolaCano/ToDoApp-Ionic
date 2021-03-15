93cf1c8832a774042a18619780bf14a785a5a892
CasasolaCano <casasolacontact@gmail.com>
2021-03-15 23:55:41 +0100

    Edit elements on the list

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
 import { WishesService } from '../../services/wishes.service';
+import { List } from '../models/list.model';
 
 
 @Component({
@@ -53,6 +54,11 @@ export class Tab1Page {
     });
 
     alert.present();
+  }
+
+  selectList(list: List) {
+    const listId = list.id;
+    this.router.navigateByUrl(`/tabs/tab1/add/${listId}`);
   }
 
 }
